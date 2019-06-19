949635df4dd8e8874ecc332e29dc9e8e23234fe4
subarna <khadka.subarnabijaya@gmail.com>
2019-06-19 12:55:34 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; //step 1: include necessary namespace
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteFile("Test.txt");
+            ReadFile("Program.cs");
+            
+        }
+        static void WriteFile(string fileName) {
+            TextWriter writer = new StreamWriter(fileName); //step 2: initialize object
+            writer.Write("Hello"); //Step 3: write something
+            writer.WriteLine("World");
+            for(int i = 1; i <= 12; i++)
+            {
+                writer.WriteLine($"{i}X3={i*3}");
+            }
+            writer.Close(); //step 4: close the object
+        }
+        static void ReadFile(string fileName)
+        {
+            TextReader reader = new StreamReader(fileName);
+            string input;
+            int lineNumber = 1;
+            while ((input=reader.ReadLine())!=null) {
+                Console.WriteLine($"{lineNumber++}\t{input}");
+            }
+            
+            
+            reader.Close();
+        }
+    }
+}
---
+++ bin/Debug/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; //step 1: include necessary namespace
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WriteFile("Test.txt");
+            ReadFile("Test.txt");
+            
+        }
+        static void WriteFile(string fileName) {
+            TextWriter writer = new StreamWriter(fileName); //step 2: initialize object
+            writer.Write("Hello"); //Step 3: write something
+            writer.WriteLine("World");
+            for(int i = 1; i <= 12; i++)
+            {
+                writer.WriteLine($"{i}X3={i*3}");
+            }
+            writer.Close(); //step 4: close the object
+        }
+        static void ReadFile(string fileName)
+        {
+            TextReader reader = new StreamReader(fileName);
+            string input;
+            while ((input=reader.ReadLine())!=null) {
+                Console.WriteLine(input);
+            }
+            
+            
+            reader.Close();
+        }
+    }
+}
